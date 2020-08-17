17f182b5e45cc73a85e5523d5aa1519a13f02bd4
belenhed <42848222+belenhed@users.noreply.github.com>
2020-08-17 09:04:54 -0400

    Update Configuration.h
    change date configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E3P FW Jul072020"
+#define CUSTOM_MACHINE_NAME "E3P BH AUG172020"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
