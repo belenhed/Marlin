55c2851755053f353b49e1b990658f7db659b9e0
belenhed <42848222+belenhed@users.noreply.github.com>
2021-04-20 11:08:00 -0400

    Stepper rate overide

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2067,7 +2067,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MAXIMUM_STEPPER_RATE 5000000
+//#define MAXIMUM_STEPPER_RATE 5000000
 
 // @section temperature
 
